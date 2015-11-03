9cc19962e29ee30d2006027a857f9d10c2379ee0
Kameron R. Sween <kameron@krsween.com>
2015-11-03 08:51:57 -0500

    fixed refactor bug

--- src/app/mockData/mockSuites.service.spec.js
+++ src/app/mockData/mockSuites.service.spec.js
@@ -9,11 +9,11 @@
     // instantiate service
     var mockSuites;
     beforeEach(inject(function (_mockSuites_) {
-      suites = _mockSuites_;
+      mockSuites = _mockSuites_;
     }));
 
     it('should have a get() method for retrieving mock data.', function () {
-      expect(suites.get).toBeDefined();
+      expect(mockSuites.get).toBeDefined();
     });
 
   });
